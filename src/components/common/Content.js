cfd8d9882485a0ee02d2b4122c283c086ca00a83
Simon Szwajca <szwajca@outlook.de>
2022-05-30 18:31:21 +0200

    Events: Add new card on click

--- src/components/common/Content.js
+++ src/components/common/Content.js
@@ -1,8 +1,49 @@
+import { useState } from "react";
+import { getRandomImageUrl } from "../../utils/ImageHelper";
 import Button from "../ui/Button";
 import Card from "../ui/Card";
 import Hero from "./Hero";
 
+const INITIAL_CARDS = [
+  {
+    id: 0,
+    title: "Image Card",
+    description: "Lorem Ipsum Dolor Sit Amet.",
+    buttonText: "Refresh",
+    imageUrl: getRandomImageUrl(),
+  },
+  {
+    id: 1,
+    title: "Image Card",
+    description: "Lorem Ipsum Dolor Sit Amet.",
+    buttonText: "Refresh",
+    imageUrl: getRandomImageUrl(),
+  },
+  {
+    id: 2,
+    title: "Image Card",
+    description: "Lorem Ipsum Dolor Sit Amet.",
+    buttonText: "Refresh",
+    imageUrl: getRandomImageUrl(),
+  },
+];
+
 function Content() {
+  const [cards, setCards] = useState(INITIAL_CARDS);
+
+  const addCard = () => {
+    setCards([
+      ...cards,
+      {
+        id: cards.length,
+        title: "Image Card",
+        description: "Lorem Ipsum Dolor Sit Amet.",
+        buttonText: "Refresh",
+        imageUrl: getRandomImageUrl(),
+      },
+    ]);
+  };
+
   return (
     <div className="content">
       <Hero
@@ -14,24 +55,17 @@ function Content() {
       <div className="container">
         <div className="card-listing">
           <div className="cards grid grid--columns-3">
-            <Card
-              buttonText="Update Image"
-              title="Image 1"
-              description="Lorem Ipsum Dolor Sit Amet."
-            />
-            <Card
-              buttonText="Update Image"
-              title="Image 2"
-              description="Lorem Ipsum Dolor Sit Amet."
-            />
-            <Card
-              buttonText="Update Image"
-              title="Image 3"
-              description="Lorem Ipsum Dolor Sit Amet."
-            />
+            {cards.map((card, idx) => (
+              <Card
+                key={card.id}
+                buttonText={card.buttonText}
+                title={`${card.title} ${idx + 1}`}
+                description={card.description}
+              />
+            ))}
           </div>
           <div className="card-listing__actions">
-            <Button size="large">Add Card</Button>
+            <Button onClick={addCard}>Add Card</Button>
           </div>
         </div>
       </div>
