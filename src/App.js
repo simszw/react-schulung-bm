8527cad59b4a01f5740a47959347d15fcf72e512
Simon Szwajca <szwajca@outlook.de>
2022-05-30 19:20:08 +0200

    Added Routing and Home-Route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import Layout from "./components/common/Layout";
+import Home from "./components/routes/Home";
 
 function App() {
   return (
     <div className="App">
-      <Layout />
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import Content from "./Content";
 import Footer from "./Footer";
 import Header from "./Header";
@@ -6,7 +7,9 @@ function Layout() {
   return (
     <div>
       <Header />
-      <Content />
+      <Content>
+        <Outlet />
+      </Content>
       <Footer />
     </div>
   );
