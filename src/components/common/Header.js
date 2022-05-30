2ea4362f649bbeac96eae9ffa00ba474b345e455
Simon Szwajca <szwajca@outlook.de>
2022-05-30 17:53:12 +0200

    Added sub-component for Header: Logo

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,7 +1,9 @@
+import Logo from "./Logo";
+
 function Header() {
   return (
     <header style={{ display: "flex", justifyContent: "space-between" }}>
-      <p>Logo</p>
+      <Logo />
       <p>Navigation</p>
       <p>Button</p>
     </header>
---
+++ src/components/common/Logo.js
@@ -0,0 +1,14 @@
+function Logo() {
+  return (
+    <div className="logo">
+      <img
+        className="logo__image"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"
+        alt="Logo"
+      />
+      <div className="logo__name">Logo</div>
+    </div>
+  );
+}
+
+export default Logo;
