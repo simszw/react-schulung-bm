6ab2bf89b1dff6c69ed84d2c932aa13bfc09160a
Simon Szwajca <szwajca@outlook.de>
2022-05-30 18:41:52 +0200

    Events: Hero click counter

--- src/components/common/Hero.js
+++ src/components/common/Hero.js
@@ -1,13 +1,26 @@
+import { useState } from "react";
 import Button from "../ui/Button";
 
 function Hero(props) {
   const { title, description, buttonText } = props;
+  const [counter, setCounter] = useState(0);
+
+  const updateCounter = () => {
+    setCounter(counter + 1);
+  };
+
   return (
     <div className="hero">
       <div className="hero__title">{title}</div>
-      <div className="hero__description">{description}</div>
+      <div className="hero__description">
+        {counter === 0
+          ? description
+          : `You clicked the button ${counter} ${
+              counter === 1 ? "time" : "times"
+            }.`}
+      </div>
       <div className="hero__button">
-        <Button size="large" variant="dark">
+        <Button size="large" variant="dark" onClick={updateCounter}>
           {buttonText}
         </Button>
       </div>
