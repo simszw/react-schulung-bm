cebc18b662f490e94dfe385f62ee26ca0efae650
Simon Szwajca <szwajca@outlook.de>
2022-05-30 18:00:20 +0200

    Added sub-component for Content: Card

--- src/components/common/Content.js
+++ src/components/common/Content.js
@@ -1,3 +1,4 @@
+import Card from "../ui/Card";
 import Hero from "./Hero";
 
 function Content() {
@@ -8,9 +9,9 @@ function Content() {
       <div className="container">
         <div className="card-listing">
           <div className="cards grid grid--columns-3">
-            <p>Card 1</p>
-            <p>Card 2</p>
-            <p>Card 3</p>
+            <Card />
+            <Card />
+            <Card />
           </div>
           <div className="card-listing__actions">
             <p>Button "Add Card"</p>
---
+++ src/components/ui/Card.js
@@ -0,0 +1,21 @@
+function Card() {
+  return (
+    <div className="card">
+      <div className="card__header">
+        <div className="card__title">Title</div>
+        <div className="card__description">Description</div>
+      </div>
+      <div className="imageContainer">
+        <img
+          src="https://picsum.photos/400?random=1"
+          alt="Random Lorem Picsum"
+        />
+      </div>
+      <div className="card__actions">
+        <p>Button</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
